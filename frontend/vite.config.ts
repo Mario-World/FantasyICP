e64d30f3619e25b2b42482f4a94e58bf949dd6db
Mario-World <pavansaigudelli01@gmail.com>
2025-06-22 04:58:39 +0530

    updated code

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig(({ mode }) => {
     define: {
       "process.env": env,
     },
+    build: {
+      emptyOutDir: true,
+    },
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
